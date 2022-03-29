e876e5142e6dad267577eb370f533f657d1e3663
bakemonostan <ehizomokhomion@gmail.com>
2022-03-29 09:32:50 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Explore from './pages/Explore';
+import ForgotPassword from './pages/ForgotPassword';
+import Offers from './pages/Offers';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>Meh</header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/forgotpassword' element={<ForgotPassword />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
