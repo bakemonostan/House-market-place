76656593907f3bdbf608d9abb6e8575277d5dadb
bakemonostan <ehizomokhomion@gmail.com>
2022-03-29 15:09:43 -0700

    sign in updated

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,16 @@
+import { useEffect, useState } from 'react';
+import { getAuth } from 'firebase/auth';
+
 function Profile() {
-  return (
-    <div>
-      <h1>Profile</h1>
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+  const auth = getAuth();
+
+  useEffect(() => {
+    setUser(auth.currentUser);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  return user ? <h1>{user.displayName}</h1> : 'Not logged In';
 }
 
 export default Profile;
--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
 import visibilityIcon from '../assets/svg/visibilityIcon.svg';
 
@@ -22,6 +23,23 @@ function SignIn() {
     }));
   };
 
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const auth = getAuth();
+      const userCredentia = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredentia.user) {
+        navigate('/');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
       <div className='pageContainer'>
@@ -29,7 +47,7 @@ function SignIn() {
           <p className='pageHeader'>Welcome Back</p>
         </header>
 
-        <form>
+        <form onSubmit={onSubmit}>
           <input
             type='email'
             className='emailInput'
